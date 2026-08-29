310e13c71734db4d9bcf229e6e4f2f47b3ce8ddf
teksite <teksite.design@gmail.com>
2026-08-29 14:56:52 +0330

    add publihs config in authotrize:install command

--- src/Console/AuthInstall.php
+++ src/Console/AuthInstall.php
@@ -17,31 +17,55 @@ class AuthInstall extends Command
      */
     public function handle(): void
     {
+        $this->copyMigration();
+        $this->publishConfigFile();
+    }
+
+    private function copyMigration(): void
+    {
+        $this->info('Publishing migrations assets.');
+
         $stubPath = __DIR__ . '/../Migrations/stub';
-
-        $stubFiles = [
-            'permission' => $stubPath . '/0001_01_01_100100__create_permissions_table.stub',
-            'role'       => $stubPath . '/0001_01_01_100101_create_roles_table.stub',
-        ];
-
         $targetPath = database_path('migrations');
 
-        if (!File::isDirectory($targetPath)) {
-            File::makeDirectory($targetPath, 0755, true);
-        }
-        $targetPaths = [
-            'permission' => $targetPath . '/0001_01_01_100100_create_permissions_table.php',
-            'role'       => $targetPath . '/0001_01_01_100101_create_roles_table.php',
+        if (!File::isDirectory($targetPath)) File::makeDirectory($targetPath, 0755, true);
+
+
+        $migrations = [
+            'permission' => [
+                'source' => $stubPath . '/0001_01_01_100100__create_permissions_table.stub',
+                'target' => $targetPath . '/0001_01_01_100100_create_permissions_table.php',
+            ],
+            'role'       => [
+                'source' => $stubPath . '/0001_01_01_100101_create_roles_table.stub',
+                'target' => $targetPath . '/0001_01_01_100101_create_roles_table.php',
+            ],
         ];
 
-        foreach ($targetPaths as $key=>$target) {
-            if (!File::exists($target)) {
-                File::copy($stubFiles[$key], $target);
-                $this->info('Created migration: ' . $target);
-            } else {
-                $this->warn('Migration already exists: ' . $target);
+        foreach ($migrations as $key=>$migration) {
+            if (File::exists($migration['target'])) {
+                $this->components->twoColumnDetail(
+                    "File [{$migration['target']}] already exists  ",
+                    '<fg=yellow;options=bold>SKIPPED</>'
+                );
+                continue;
             }
 
+            File::copy($migration['source'], $migration['target']);
+
+            $this->components->twoColumnDetail(
+                "Copying $key migration file to [{$migration['target']}]",
+                '<fg=green;options=bold>DONE</>'
+            );
+
         }
+    }
+
+    private function publishConfigFile()
+    {
+        return $this->call('vendor:publish', [
+            '--tag'   => 'authorize-config',
+            '--force' => false,
+        ]);
     }
 }
